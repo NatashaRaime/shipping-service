4c23320c6ca81edcdd1944f794712e1425150f65
Natasha Ewing <natasharai@me.com>
2016-11-10 15:58:08 -0800

    timeout and logger.info added

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,15 +1,25 @@
+require 'timeout'
 class ShipmentsController < ApplicationController
 
   def create
-    shipment = Shipment.new(params[:shipment][:origin], params[:shipment][:destination], params[:shipment][:packages])
+    logger.info "#{params}"
 
-    ups_rates = Shipment.ups(shipment.origin, shipment.destination, shipment.packages)
+    status = Timeout::timeout(10) {
+  # Something that should be interrupted if it takes more than 5 seconds...
 
-    usps_rates = Shipment.usps(shipment.origin, shipment.destination, shipment.packages)
+      shipment = Shipment.new(params[:shipment][:origin], params[:shipment][:destination], params[:shipment][:packages])
 
-    render json: {"ups": ups_rates, "usps": usps_rates}, status: :created
+      ups_rates = Shipment.ups(shipment.origin, shipment.destination, shipment.packages)
+
+      usps_rates = Shipment.usps(shipment.origin, shipment.destination, shipment.packages)
+
+      render json: {"ups": ups_rates, "usps": usps_rates}, status: :created }
+
+    if status == false
+      redirect_to :timeout
+    end
+
   end
-
   #private
 
   # def shipment_params
